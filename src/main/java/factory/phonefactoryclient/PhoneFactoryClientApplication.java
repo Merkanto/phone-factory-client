cb32620b21df8420ee8377b46dbb110136b6b63d
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-13 18:14:28 +0300

    Initial project commit.

---
+++ src/main/java/factory/phonefactoryclient/PhoneFactoryClientApplication.java
@@ -0,0 +1,13 @@
+package factory.phonefactoryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneFactoryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneFactoryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/factory/phonefactoryclient/PhoneFactoryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package factory.phonefactoryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneFactoryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
