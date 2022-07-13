acf97613692a9fa937753e7fa4ce2dde4273ccca
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-13 23:19:06 +0300

    Add add, save, update and delete methods and tests for phone and customer.

--- src/main/java/factory/phonefactoryclient/web/client/PhoneFactoryClient.java
+++ src/main/java/factory/phonefactoryclient/web/client/PhoneFactoryClient.java
@@ -1,11 +1,13 @@
 package factory.phonefactoryclient.web.client;
 
+import factory.phonefactoryclient.web.model.CustomerDto;
 import factory.phonefactoryclient.web.model.PhoneDto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @ConfigurationProperties(prefix = "phone.factory", ignoreUnknownFields = false)
@@ -13,6 +15,7 @@ import java.util.UUID;
 public class PhoneFactoryClient {
 
     public final String PHONE_PATH_V1 = "/api/v1/phone/";
+    public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private String apihost;
 
     private final RestTemplate restTemplate;
@@ -25,8 +28,35 @@ public class PhoneFactoryClient {
         return restTemplate.getForObject(apihost + PHONE_PATH_V1 + uuid.toString(), PhoneDto.class);
     }
 
+    public URI saveNewPhone(PhoneDto phoneDto) {
+        return restTemplate.postForLocation(apihost + PHONE_PATH_V1, phoneDto);
+    }
+
+    public void updatePhone(UUID uuid, PhoneDto phoneDto) {
+        restTemplate.put(apihost + PHONE_PATH_V1 + uuid.toString(), phoneDto);
+    }
+
+    public void deletePhone(UUID uuid) {
+        restTemplate.delete(apihost + PHONE_PATH_V1 + uuid);
+    }
+
     public void setApihost(String apihost) {
         this.apihost = apihost;
     }
 
+    public CustomerDto getCustomerById(UUID customerId) {
+        return restTemplate.getForObject(apihost + CUSTOMER_PATH_V1 + customerId.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apihost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        restTemplate.put(apihost + CUSTOMER_PATH_V1 + customerId, customerDto);
+    }
+
+    public void deleteCustomer(UUID customerId) {
+        restTemplate.delete(apihost + CUSTOMER_PATH_V1 + customerId);
+    }
 }
--- src/test/java/factory/phonefactoryclient/web/client/PhoneFactoryClientTest.java
+++ src/test/java/factory/phonefactoryclient/web/client/PhoneFactoryClientTest.java
@@ -1,10 +1,12 @@
 package factory.phonefactoryclient.web.client;
 
+import factory.phonefactoryclient.web.model.CustomerDto;
 import factory.phonefactoryclient.web.model.PhoneDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,5 +22,66 @@ class PhoneFactoryClientTest {
         PhoneDto dto = client.getPhoneById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewPhone() {
+        PhoneDto phoneDto = PhoneDto.builder()
+                .phoneName("New Phone")
+                .build();
+
+        URI uri = client.saveNewPhone(phoneDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdatePhone() {
+        PhoneDto phoneDto = PhoneDto.builder()
+                .phoneName("New Phone")
+                .build();
+
+        client.updatePhone(UUID.randomUUID(), phoneDto);
+    }
+
+    @Test
+    void testDeletePhone() {
+        client.deletePhone(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder()
+                .name("Goshko")
+                .build();
+
+        URI uri = client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder()
+                .name("Ivan")
+                .build();
+
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
